0c252359313b14b5f228e19f2e82fb12a182a869
tuffer020 <tuffer020@gmail.com>
2015-05-11 19:15:14 -0400

    Added a splash screen Still needs some work. The picture can be changed/photoshopped

---
+++ app/src/main/java/com/example/work/studyroom/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.work.studyroom;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.work.studyroom.R;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
